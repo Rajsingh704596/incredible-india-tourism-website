55a0627bbc064ff83da1c8b605d2d35ddbf6df8d
Raj <rajsingh704596@gmail.com>
2025-08-22 20:23:04 +0530

    config: add deployed backend URL to .env for frontend API calls

--- Frontend/src/API/Axios.jsx
+++ Frontend/src/API/Axios.jsx
@@ -1,14 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:3000/api"
-})
+  // baseURL:"http://localhost:3000/api"                    // when in local system run
 
+  baseURL: `${import.meta.env.VITE_APP_URI_API}/api`,
+});
 
-export const getReq=()=>{
-    return api.get("/data");
-}
+export const getReq = () => {
+  return api.get("/data");
+};
 
-export const getIndReq=(name)=>{
-    return api.get(`/data?search=${name}`)
-}
+export const getIndReq = (name) => {
+  return api.get(`/data?search=${name}`);
+};
